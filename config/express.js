a0e9545e7984d783b6dd72e3c61d0add584b13f8
nandobcr <nandobcr@gmail.com>
2016-09-27 23:11:21 -0300

    Quarto commit

--- config/express.js
+++ config/express.js
@@ -19,5 +19,19 @@ module.exports = function() {
         .then('infra')
         .into(app);
 
+    app.use(function(req,res,next){
+        res.status(404).render('erros/404');
+        next();
+    });
+
+    app.use(function(error,req,res,next){
+        if (process.env.NODE_ENV == 'production') {
+            console.log(error);
+            res.status(500).render('erros/500');
+            return;
+        }
+        next(error);
+    });        
+
     return app;
 }
